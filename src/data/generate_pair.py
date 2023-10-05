cb05021f412225e54ef7a0f4cbfdef6786c7f70b
lvtan71 <levantan0701@gmail.com>
2023-10-05 22:46:00 +0700

    fix error

--- src/data/generate_pair.py
+++ src/data/generate_pair.py
@@ -60,10 +60,10 @@ if __name__=="__main__":
 
     pairs = list()
     for line in data:
-        line["claim"] = re.sub(r"\.", "", line["claim"])
-        claim = " ".join(line["claim"].strip().split())
-        line["evidence"] = re.sub(r"\.", "", line["evidence"])
-        evidence = " ".join(line["evidence"].strip().split())
+        claim = re.sub(r"\.", "", line["claim"])
+        claim = " ".join(claim.strip().split())
+        evidence = re.sub(r"\.", "", line["evidence"])
+        evidence = " ".join(evidence.strip().split())
         context = line["context"]
         context = [re.sub(r"\.", "", sent) for sent in context]
         for sentence in context:
