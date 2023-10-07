97eabb283280f5ee1bf60354c6946c8823dac56f
lvtan71 <levantan0701@gmail.com>
2023-10-07 20:18:18 +0700

    fix output

--- src/model/test.py
+++ src/model/test.py
@@ -23,9 +23,8 @@ def save_to_file(all_predict, outpath):
     with open(outpath, "w", encoding="utf-8") as out:
         for key, values in all_predict.items():
             sorted_values = sorted(values, key=lambda x:x[-1], reverse=True)
-            #data = json.dumps({"id": key, "evidence": sorted_values[:5]}) 
-            #out.write(data + "\n")
-            json.dumps({"id": key, "evidence": sorted_values[:5]}, out, ensure_ascii=False, indent=4)
+            data = json.dumps({"id": key, "evidence": sorted_values[:5]}, ensure_ascii=False, indent=4) 
+            out.write(data + "\n")
 
 
 
