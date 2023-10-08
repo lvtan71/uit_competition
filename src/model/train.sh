1edb0bdfd008c7271fffedf96d1f4f1b42e0840d
lvtan71 <levantan0701@gmail.com>
2023-10-08 15:58:45 +0700

    adjust .sh file

--- src/model/train.sh
+++ src/model/train.sh
@@ -1,3 +1,8 @@
-!python model/train.py --outdir checkpoint/retrieval_model \
+python model/train.py --outdir checkpoint/retrieval_model \
 --train_path data/pairs_data_train \
---valid_path data/pairs_data_valid
+--valid_path data/pairs_data_valid \
+--bert_pretrain vinai/phobert-base-v2 \
+--num_train_epochs 10 \
+--eval_step 200 \
+--max_len 256 \
+--gradient_accumulation_steps 1
